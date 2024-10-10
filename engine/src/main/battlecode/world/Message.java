095e9c1adeb22e5f7c29effe8ac6f6310081c94b
bossbobster <bobby.kostin@gmail.com>
2024-10-10 17:17:49 -0400

    changed char type to byte

--- engine/src/main/battlecode/world/Message.java
+++ engine/src/main/battlecode/world/Message.java
@@ -5,12 +5,12 @@ import battlecode.common.GameConstants;
 public class Message {
     private static int globalID = 0;
     private int id;
-    private char[] bytes;
+    private byte[] bytes;
 
-    public Message(char[] bytes){
+    public Message(byte[] bytes){
         globalID ++;
         this.id = globalID;
-        this.bytes = new char[MAX_MESSAGE_BYTES];
+        this.bytes = new byte[MAX_MESSAGE_BYTES];
         for(int i = 0; i < Math.min(MAX_MESSAGE_BYTES, bytes.length); i ++)
             this.bytes[i] = bytes[i];
     }
@@ -19,7 +19,7 @@ public class Message {
         return id;
     }
 
-    public char[] getBytes(){
+    public byte[] getBytes(){
         return this.bytes;
     }
 
