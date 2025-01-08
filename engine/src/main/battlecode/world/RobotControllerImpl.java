439f957be1546ace0c428f7635732430e0c81965
Ian Gonzalez Hermosillo <iangh@mit.edu>
2025-01-08 01:50:37 -0500

    completing tower patterns costs chips

--- engine/src/main/battlecode/world/RobotControllerImpl.java
+++ engine/src/main/battlecode/world/RobotControllerImpl.java
@@ -708,6 +708,20 @@ public final class RobotControllerImpl implements RobotController {
         this.gameWorld.markResourcePattern(getTeam(), loc, rotationAngle, reflect);
     }
 
+    private UnitType getBaseUnitType(UnitType type){
+        UnitType baseType;
+        switch (type){
+            case LEVEL_TWO_DEFENSE_TOWER: baseType = UnitType.LEVEL_ONE_DEFENSE_TOWER; break;
+            case LEVEL_THREE_DEFENSE_TOWER: baseType = UnitType.LEVEL_ONE_DEFENSE_TOWER; break;
+            case LEVEL_TWO_MONEY_TOWER: baseType = UnitType.LEVEL_ONE_MONEY_TOWER; break;
+            case LEVEL_THREE_MONEY_TOWER: baseType = UnitType.LEVEL_ONE_MONEY_TOWER; break;
+            case LEVEL_TWO_PAINT_TOWER: baseType = UnitType.LEVEL_ONE_PAINT_TOWER; break;
+            case LEVEL_THREE_PAINT_TOWER: baseType = UnitType.LEVEL_ONE_PAINT_TOWER; break;
+            default: baseType = type;
+        }
+        return baseType;
+    }
+
     private void assertCanCompleteTowerPattern(UnitType type, MapLocation loc) throws GameActionException {
         assertIsRobotType(this.robot.getType());
         assertIsTowerType(type);
@@ -723,6 +737,12 @@ public final class RobotControllerImpl implements RobotController {
             throw new GameActionException(CANT_DO_THAT,
                     "Cannot complete tower pattern centered at (" + loc.x + ", " + loc.y
                             + ") because the center is not a ruin");
+        }
+
+        if (getMoney() < getBaseUnitType(type).moneyCost){
+            throw new GameActionException(CANT_DO_THAT,
+            "Cannot complete tower pattern centered at (" + loc.x + ", " + loc.y
+                    + ") because the team does not have enough money!"); 
         }
 
         if (!this.gameWorld.isValidPatternCenter(loc)) {
@@ -768,6 +788,7 @@ public final class RobotControllerImpl implements RobotController {
         assertCanCompleteTowerPattern(type, loc);
         this.gameWorld.completeTowerPattern(getTeam(), type, loc);
         InternalRobot tower = this.gameWorld.getRobot(loc);
+        this.gameWorld.getTeamInfo().addMoney(getTeam(), -getBaseUnitType(type).moneyCost);
         this.gameWorld.getMatchMaker().addSpawnAction(tower.getID(), loc, tower.getTeam(), type);
         this.gameWorld.getMatchMaker().addBuildAction(tower.getID());
     }
